48416cdc899c359d328588b369c4d69b82875862
Filma Bem <filmab.emtv2@gmail.com>
2023-02-11 21:28:36 +0000

    Added first variables

--- joma.py
+++ joma.py
@@ -5,3 +5,33 @@ import os
 import platform
 import subprocess
 
+# Locations of package managers
+
+home = os.path.expanduser("~")
+wget = home + '/AppData/Local/Microsoft/WindowsApps/winget.exe'
+scoop = home  + '/scoop/apps/scoop/current/'
+pml = [wget, 'C:\\ProgramData\\chocolatey\\bin\\choco.exe', scoop]
+
+# Basic functions
+
+def jomainstall():
+    print('code for this function')
+    
+def jomaremove():
+    print('code for this function')
+        
+def jomaupdate():
+    print('code for this function')
+    
+def jomasearch():
+    print('code for this function')
+    
+def jomaexport():
+    print('code for this function')
+    
+def jomaimport():
+    print('code for this function')
+
+def jomaupgrade():
+    print('code for this function')
+    
