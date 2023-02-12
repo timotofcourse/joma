8d821553c340b54bfc01e86cd9b0b5aa9202b0d8
Filma Bem <filmab.emtv2@gmail.com>
2023-02-12 13:00:23 +0000

    Fixed subprocess for choco process

--- joma.py
+++ joma.py
@@ -85,7 +85,8 @@ else:
     print('Chocolatey not detected')
     installchocoask = input('Do you want to install it? [Y/n]: ')
     if installchocoask == '' or 'y' or 'Y':
-        subprocess.Popen("[System.Net.ServicePointManager]::SecurityProtocol", "=",  "[System.Net.ServicePointManager]::SecurityProtocol", "-bor", "3072;", "iex", "((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))").wait()
+        installchoco = subprocess.Popen("[System.Net.ServicePointManager]::SecurityProtocol", "=",  "[System.Net.ServicePointManager]::SecurityProtocol", "-bor", "3072;", "iex", "((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))")
+        installchoco.wait()
         enablecmd2 = True
     elif installchocoask == 'n' or 'N':
         print('we will not install it')
