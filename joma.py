9c5d13be90524143485633d5856a27832e562c70
Filma Bem <filmab.emtv2@gmail.com>
2023-02-28 11:23:03 +0000

    Added verification for installed packages

--- joma.py
+++ joma.py
@@ -5,3 +5,8 @@ import shutil
 import time
 import subprocess
 
+# Check for existence of the installed package list
+
+if not os.path.exists('installed.jamo'):
+    f = open("installed.jamo", "x")
+
