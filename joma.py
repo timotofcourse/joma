e152a44ed4a356fcaeb7f67a81b8ad5eea4bd0b2
Filma Bem <filmab.emtv2@gmail.com>
2023-09-22 18:02:56 +0100

    Removed the rest of old code to re-write the whole software

--- joma.py
+++ joma.py
@@ -1,12 +1,9 @@
+#!/usr/share/python
+
 import argparse
 import os
 import sys
 import shutil
 import time
-import subprocess
-
-# Check for existence of the installed package list
-
-if not os.path.exists('installed.jamo'):
-    f = open("installed.jamo", "x")
+import threading
 
