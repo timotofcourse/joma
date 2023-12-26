a2df7949d0a9abd6159f1fde72221f1d6fbac0c9
Filma Bem <filmab.emtv2@gmail.com>
2023-12-26 11:42:44 +0000

    Added some blank lines

--- joma.py
+++ joma.py
@@ -41,62 +41,97 @@ args = parser.parse_args()
 def install_packages(packages):
 
     if packages:
+
         package_list = ' '.join(packages)
+
         try:
+
             os.system(f'sudo pacman -S --needed --noconfirm {package_list}')
+
         except Exception as e:
+
             print(f'Error: Can\'t find the packages {package_list} {e} on Pacman repos, trying AUR...')
 
             try:
+
                 os.system(f'yay -S --needed --noconfirm {package_list}')
+
             except Exception as e:
+
                 print(f'Error: Can\'t find the packages {package_list} {e} on AUR... Trying flatpak')
 
                 try:
+
                     os.system(f'flatpak install {package_list} -y')
+
                 except Exception as e:
+
                     print(f'Error: Can\'t find {package_list} on any flatpak repo')
     else:
+
         print("No packages provided for installation")
 
 def remove_packages(packages):
 
     if packages:
+
         package_list = ' '.join(packages)
+
         try:
+
             os.system(f'sudo pacman -R --needed --noconfirm{package_list}')
+
         except Exception as e:
+
             print(f'Error: Can\'t find the packages {package_list} {e} on Pacman repos, trying AUR...')
 
             try:
+
                 os.system(f'yay -R --needed --noconfirm {package_list}')
+
             except Exception as e:
+
                 print(f'Error: Can\'t find the packages {package_list} {e} on AUR... Trying flatpak')
 
                 try:
+
                     os.system(f'flatpak remove {package_list} -y')
+
                 except Exception as e:
+
                     print(f'Error: Can\'t find {package_list} on any flatpak repo')
+
     else:
+
         print("No packages provided for installation")
 
 def remove_packages_with_deps(packages):
 
     if packages:
+
         package_list = ' '.join(packages)
+
         try:
+
             os.system(f'sudo pacman -Rcnss --needed --noconfirm{package_list}')
+
         except Exception as e:
+
             print(f'Error: Can\'t find the packages {package_list} {e} on Pacman repos, trying AUR...')
 
             try:
+
                 os.system(f'yay -Rcnss --needed --noconfirm {package_list}')
+
             except Exception as e:
+
                 print(f'Error: Can\'t find the packages {package_list} {e} on AUR... Trying flatpak')
 
                 try:
+
                     os.system(f'flatpak remove {package_list} -y')
                     print(f'{package_list} Removed')
+
                 except Exception as e:
 
                     print(f'Packages {package_list} not found')
@@ -117,21 +152,32 @@ def flat_pkgs_upgrade():
 def search(packages):
 
     package_list = ' '.join(packages)
+
     try:
+
         os.system(f'sudo pacman -Ss {package_list}')
+
     except Exception as e:
+
         print(f'Error: Can\'t find the packages {package_list} {e} on Pacman repos, trying AUR...')
 
         try:
+
             os.system(f'yay -Ss {package_list}')
+
         except Exception as e:
+
             print(f'Error: Can\'t find the packages {package_list} {e} on AUR... Trying flatpak')
 
             try:
+
                 os.system(f'flatpak search {package_list} -y')
+
             except Exception as e:
+
                 print(f'Error: Can\'t find {package_list} on any flatpak repo')
     else:
+        
         print(f"No packagesavailable for {package_list}")
 
 
